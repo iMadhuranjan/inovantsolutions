778aec5f89de355cf9e87caba562c592c8fc4b99
Kushwaha Madhuranjan <72127246+iMadhuranjan@users.noreply.github.com>
2025-07-13 10:51:37 +0530

    Update dbConnect.js

--- backend/database/dbConnect.js
+++ backend/database/dbConnect.js
@@ -1,40 +1,41 @@
 // dbConnect.js
-// import mysql from 'mysql2/promise';
 
-// let connection;
-
-// export async function connectDB() {
-//   if (!connection) {
-//     connection = await mysql.createConnection({
-//       host: 'localhost',
-//       user: 'root',
-//       password: 'Ritesh@709',
-//       database: 'my_database',
-//     });
-//     console.log('Connected to MySQL');
-//   }
-//   return connection;
-// }
-
-
-// Above are for Localhost 
-
-// Below SQl are of My Hostinger Served Hosting Server 
-
- import mysql from 'mysql2/promise';
+import mysql from 'mysql2/promise';
 
 let connection;
 
 export async function connectDB() {
   if (!connection) {
     connection = await mysql.createConnection({
-      host: 'srv1675.hstgr.io',  
-      user: 'u743509934_admin',
+      host: 'localhost',
+      user: 'root',
       password: 'Ritesh@709',
-      database: 'u743509934_product',
-      port: 3306,
+      database: 'my_database',
     });
     console.log('Connected to MySQL');
   }
   return connection;
 }
+
+
+// Above are for Localhost 
+
+// Below SQl are of My Hostinger Served Hosting Server 
+
+//  import mysql from 'mysql2/promise';
+
+// let connection;
+
+// export async function connectDB() {
+//   if (!connection) {
+//     connection = await mysql.createConnection({
+//       host: 'srv1675.hstgr.io',  
+//       user: 'u743509934_admin',
+//       password: 'Ritesh@709',
+//       database: 'u743509934_product',
+//       port: 3306,
+//     });
+//     console.log('Connected to MySQL');
+//   }
+//   return connection;
+// }
